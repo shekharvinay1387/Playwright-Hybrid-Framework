86892f2d6ffbf17cc07088579de979b49e12e9e9
Vinay Shekhar <shekharvinay1387@gmail.com>
2026-06-30 09:07:29 +0530

    Initial project setup with Playwright framework

---
+++ playwright.config.js
@@ -0,0 +1,52 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+
+  // Test folder
+  testDir: './tests',
+
+  // Run tests in parallel
+  fullyParallel: true,
+
+  // Fail build if test.only is committed in CI
+  forbidOnly: !!process.env.CI,
+
+  // Retry failed tests only in CI
+  retries: process.env.CI ? 2 : 0,
+
+  // Number of workers in CI
+  workers: process.env.CI ? 1 : undefined,
+
+  // HTML Report
+  reporter: 'html',
+
+  // Common settings
+  use: {
+
+    // Application URL
+    baseURL: 'https://automationexercise.com',
+
+    // Open browser in headed mode
+    headless: false,
+
+    // Capture screenshot only when test fails
+    screenshot: 'only-on-failure',
+
+    // Save video only for failed tests
+    video: 'retain-on-failure',
+
+    // Save trace on first retry
+    trace: 'on-first-retry',
+  },
+
+  // Browser
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+});
